28c5e05f937221f95c8d07251f6bde23905eaa55
Mahboob Mustafa <mahboobmustafa54@gmail.com>
2021-07-26 19:47:25 +0500

    fixed int_classes to int instead of pulling from six

--- utils/collate.py
+++ utils/collate.py
@@ -5,8 +5,9 @@ Licensed under the CC BY-NC 4.0 license (https://creativecommons.org/licenses/by
 import torch
 import numpy as np
 import collections
-from torch._six import string_classes, int_classes
+from torch._six import string_classes
 
+int_classes = int
 
 """ Custom collate function """
 def collate_custom(batch):
